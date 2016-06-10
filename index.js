2ffb29e186bf1b25627c0c927a9b82c4ce78106d
François Parmentier <francois.parmentier@gmail.com>
2016-06-10 14:04:13 +0200

    :sparkles: Add contentMore attribute

--- index.js
+++ index.js
@@ -24,25 +24,45 @@ function LodexWidget(id = null, classe = null) {
 
     const selector = `#${_id} .${_class}`;
     const items = sektor(selector);
-    if (!items.length) { console.log('Nothing found'); return; }
+    if (!items.length) { console.error(`Selector ${selector} does not match any DOM element!`); return; }
+
     for(let item of items) {
       const { innerText: value,
             attributes: { about: { value: uri }} }
         = item;
 
+      const properties = new Map([
+        ["contentText", "http://www.w3.org/2008/05/skos-xl#prefLabel"],
+        ["contentMore", "http://www.w3.org/2004/02/skos/core#definition"]
+      ]);
+
+      const selector = '#article-types .facet[about="'+ uri +'"]';
+      console.log('selector', selector);
+      const tooltipOptions = {
+        targetSelector: selector
+      };
+
       aja()
       .url(uri + "?alt=jsonld")
       .on(200, function (response) {
-        JBJ.render({
-          getJsonLdField: "http://www.w3.org/2008/05/skos-xl#prefLabel"
-        }, response[0], function (err, res) {
-          const selector = '#article-types .facet[about="'+ uri +'"]';
-          console.log('selector', selector);
-          html5tooltips({
-            targetSelector: selector,
-            contentText: res.content
+
+        for (let [option, property] of properties) {
+          JBJ.render({
+            getJsonLdField: property
+          }, response[0], function (err, res) {
+            if (err) { console.error(err); return; }
+
+            tooltipOptions[option] = res.content;
+            properties.delete(option);
+            console.log('properties.size',properties.size)
+            if (properties.size) { return; }
+
+            console.log('tooltipOptions', tooltipOptions)
+            html5tooltips(tooltipOptions);
           });
-        });
+
+        }
+
       })
       .go();
 
